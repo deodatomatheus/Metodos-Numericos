ab540cdb469421f9cbde836378fb5ac66d9ca2cc
Matheus Deodato de Oliveira <mdo2@cin.ufpe.br>
2018-08-26 20:14:39 -0300

    feat: add euler's method
    Graphics generation implemented

--- imp_met.py
+++ imp_met.py
@@ -1,4 +1,33 @@
-arq = open('entrada.txt', 'r')
+import parser  
+import matplotlib.pyplot as plt
+
+def euler(param):
+	print('Metodo de Euler')
+	y = float(param[1])
+	t = float(param[2])	
+	h = float(param[3])
+	n = int(param[4])
+	f = parser.expr(param[5]).compile()
+	lista_t = []
+	lista_y = []
+	print('y(',t,') =',y)
+	print('h =', h)
+	for j in range(n+1):
+		print(j, " " , y)
+		lista_y.append(y)
+		lista_t.append(t)
+		k = eval(f)
+		y = y + h * k
+		t = t + h
+	return lista_t, lista_y
+
+		
+arq = open('input.txt', 'r')
 for linha in arq:
-	print(linha)
+	param = linha.split()
+	if(param[0]=="euler"):
+		graphic_t, graphic_y  = euler(param)
+		plt.plot(graphic_t, graphic_y)
+		plt.show()
+
 arq.close()
